50bfdd8c408af3e1ac749384896391720550dd7e
Nicholas Alencar <alencar.nicholas@gmail.com>
2024-06-10 21:45:09 -0300

    Redirecionando

---
+++ PrjHospital/PrjHospital/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace PrjHospital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnForgetei = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogar = new System.Windows.Forms.PictureBox();
+            this.btnEye = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEye)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPassword.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.txtPassword.Location = new System.Drawing.Point(23, 220);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(212, 31);
+            this.txtPassword.TabIndex = 11;
+            this.txtPassword.Text = "a";
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            // 
+            // btnForgetei
+            // 
+            this.btnForgetei.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnForgetei.AutoSize = true;
+            this.btnForgetei.Font = new System.Drawing.Font("Poppins SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgetei.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnForgetei.Location = new System.Drawing.Point(70, 303);
+            this.btnForgetei.Name = "btnForgetei";
+            this.btnForgetei.Size = new System.Drawing.Size(128, 19);
+            this.btnForgetei.TabIndex = 10;
+            this.btnForgetei.Text = "Esqueci minha senha";
+            this.btnForgetei.Click += new System.EventHandler(this.btnForgetei_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEmail.BackColor = System.Drawing.SystemColors.Window;
+            this.txtEmail.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(23, 160);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(212, 31);
+            this.txtEmail.TabIndex = 9;
+            this.txtEmail.Text = "a";
+            this.txtEmail.Click += new System.EventHandler(this.txtEmail_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(121)))), ((int)(((byte)(96)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btnLogar);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.btnEye);
+            this.panel1.Controls.Add(this.btnForgetei);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 489);
+            this.panel1.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImage = global::PrjHospital.Properties.Resources.logoDark_1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(93, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogar.BackgroundImage = global::PrjHospital.Properties.Resources.Group_65;
+            this.btnLogar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLogar.Location = new System.Drawing.Point(108, 397);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(45, 45);
+            this.btnLogar.TabIndex = 14;
+            this.btnLogar.TabStop = false;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // btnEye
+            // 
+            this.btnEye.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnEye.BackColor = System.Drawing.Color.White;
+            this.btnEye.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnEye.BackgroundImage")));
+            this.btnEye.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEye.Location = new System.Drawing.Point(203, 223);
+            this.btnEye.Name = "btnEye";
+            this.btnEye.Size = new System.Drawing.Size(25, 25);
+            this.btnEye.TabIndex = 13;
+            this.btnEye.TabStop = false;
+            this.btnEye.Click += new System.EventHandler(this.btnEye_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PrjHospital.Properties.Resources.vector;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 488);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela de Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEye)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox btnLogar;
+        private System.Windows.Forms.PictureBox btnEye;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label btnForgetei;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ PrjHospital/PrjHospital/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrjHospital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtEmail.Text = "Insira seu e-mail";
+            txtEmail.ForeColor = Color.Gray;
+            txtPassword.Text = "Insira sua senha";
+            txtPassword.ForeColor = Color.Gray;
+
+            txtPassword.PasswordChar = '\0';
+        }
+
+        private void btnForgetei_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("https://google.com");
+            Process.Start(sInfo);
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkEye()
+        {
+            if (txtPassword.PasswordChar == '*')
+            {
+                txtPassword.PasswordChar = '\0';
+                btnEye.BackgroundImage = Properties.Resources.zoio;
+            }
+            else if (txtPassword.PasswordChar == '\0')
+            {
+                btnEye.BackgroundImage = Properties.Resources.dormi;
+
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void btnEye_Click(object sender, EventArgs e)
+        {
+            checkEye();
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+            txtEmail.Text = "";            
+            txtEmail.ForeColor = Color.Black;
+            TextBox txt = (sender as TextBox);
+            String texto = txt.Text;
+            if (texto.StartsWith("Insira"))
+            {
+                txt.Text = "";
+            }
+
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+            txtPassword.ForeColor = Color.Black;
+            txtPassword.PasswordChar = '*';
+            btnEye.BackgroundImage = Properties.Resources.dormi;
+            txtPassword.PasswordChar = '*';
+
+        }
+
+
+
+    }
+}
